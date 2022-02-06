4274d6231c082acbef27dd40702d0698a4b53c14
Andrea Maccan <acmaccan@gmail.com>
2022-02-06 19:39:48 -0300

    make it better

--- js/server.js
+++ js/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const axios = require('axios').default;
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT;
 
 app.use(cors({ origin: '*' }));
 
